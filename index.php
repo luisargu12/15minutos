85dacaa3ae75cdf1e4b366970a2bd350f022af6e
Luis Arguello <luis.argu00@gmail.com>
2026-05-11 12:07:00 -0600

    Primer push

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<h1>Prueba</h1>
+<h1>Hola Mundo</h1>
+<p>Este es un párrafo</p>
+<a href="https://www.google.com">Google</a>
+<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png" alt="Google Logo">
+<button>Click</button>
+<input type="text">
+<textarea></textarea>
+<select>
+    <option>Option 1</option>
+    <option>Option 2</option>
+    <option>Option 3</option>
+</select>
+<form>
+    <input type="text">
+    <input type="password">
+    <input type="submit">
+</form>
+<table border="1">
+    <tr>
+        <th>Header 1</th>
+        <th>Header 2</th>
+    </tr>
+    <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+    </tr>
+</table>
